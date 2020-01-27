9d3ac9f24da1c904f7f54d99a2a38b64526ca8cc
pr0videncespr0xy <59985153+pr0videncespr0xy@users.noreply.github.com>
2020-01-27 09:12:30 -0500

    Update Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,32 +1,37 @@
+#Import random module
 import random
 
-#game set up
-print("Welcome to the guessing game partner of mine")
-number_of_guesses = 3 #User has 3 guesses before losing the game
+#Print out greeting
+print("Welcome to the guessing game!!!")
 
-#Computer guesses a random int between 1 and 10
+#set number of guesses and define a variable for the user winning
+number_of_guess = 3
+user_won = False
+
+#Set answer
 correct_answer = random.randint(1, 10)
 
-while number_of_guesses > 0:
-    #User guesses the number
-    user_guess = input("guess my number: ")
-    user_guess = int(user_guess)
+#set number of guesses and set assign variable as integer
+while number_of_guess > 0 :
+    input("Guess my number : ")
+    number_of_guess = int(number_of_guess)
 
-    #Computer tells user if the guess was too high or too low
-    if user_guess == correct_answer:
-        print("Good guess!")
-        print("You are right! Congratulations!!!!")
+#set logic for guessing game
+    if number_of_guess == correct_answer:
+        print("congratulations!!!")
         user_won = True
         break
-    elif user_guess > correct_answer:
-        print("You went over try, something a little lower")
-    elif user_guess < correct_answer:
-        print("WOAH DERE, Too low scrub, try again")
-
-    number_of_guesses -= 1
-
+    elif number_of_guess > correct_answer:
+        print("you guessed too high")
+    elif number_of_guess < correct_answer:
+        print("you guessed too low")
+        
+#set the negation of the number of guesses after every attempt
+    number_of_guess -= 1
+    
+#set the dialogue of the end result
 if user_won == True:
-    print("Congrats friend you have won!!")
+    print("congratulations you have won")
 else:
-    print("Sorry buddy, but you lose.")
+    print("Sorry, but you lose.")
 
